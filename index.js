863e7e9153cdc0a69522da64127cc93fecc36687
Your Name <aujones95@gmail.com>
2019-04-23 17:43:27 -0400

    finished javascript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-nav.addEventListener('click', () => style.color='blue');
+const nav = document.querySelector('.home');
+// console.log(nav)
+nav.addEventListener('click', (event) => {
+  // console.log(event)
+ console.log(nav.style);
+ nav.style.color = "blue";
+
+
+})
+
+const button = document.getElementById('btn');
+
+button.addEventListener('mouseover',function(e){
+    event.target.style.color ="black";
+})
+
+
+
+
+
+
+
